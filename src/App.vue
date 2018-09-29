574ab2a41b75f04e40cc5e49f59ccadec2ccfc46
SiNcSaD <generation0987740903@gmail.com>
2018-09-29 22:43:37 +0800

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,55 @@
 <template>
-  <div class="bg">
-    <div>
+  <div>
+    <!-- <div>
        <img src="/static/Logo.jpg" alt="LOGO">
+    </div> -->
+    <div class="uk-height-large uk-flex uk-flex-center uk-flex-middle uk-background-cover" data-src="/static/Logo.jpg" uk-img>
+      <h1>The Simpsons Character Classification Application</h1>
     </div>
-    <div class="uk-container uk-container-expand">
-      <!-- Upload Picture -->
-      <div class="uk-child-width-expand@s uk-text-center uk-padding" uk-grid>
-        <div>
-            <div v-if="!image">
-              <div class="uk-placeholder uk-position-relative">
-                <h2 class="uk-position-center">Select an image</h2>
-              </div>
-              <div uk-form-custom>
-                <input type="file" @change="onFileChange" multiple>
-                <button class="uk-button uk-button-secondary" type="button" tabindex="-1">Upload</button>
-              </div>
+    <div class="uk-container uk-container-large uk-text-center uk-margin">
+
+      <div v-if="!image">
+        <div class="uk-placeholder">
+          <h2 class="uk-position-center">Select an image</h2>
+        </div>
+        <div uk-form-custom>
+          <input type="file" @change="onFileChange" multiple>
+          <button class="uk-button uk-button-secondary" type="button" tabindex="-1">Upload</button>
+        </div>
+      </div>
+
+      <div v-else>
+        <div class="uk-child-width-expand@s uk-flex-middle" uk-grid>
+
+          <div class="uk-width-1-2">
+            <img id="img_src" :src="image">
+          </div>
+
+          <div v-show="isPredicted" class="uk-card uk-card-default uk-card-body character_introduction">
+            <!-- 年齡標籤，Male or Female -->
+            <div v-if="character_gender === 'Male'">
+              <div class="uk-card-badge uk-label">Maasdsale</div>
             </div>
             <div v-else>
-              <div class="uk-placeholder uk-position-relative">
-                <img class="uk-position-center" id="img_src" :src="image" alt="uploadImage" style="max-height: 300px;">
-              </div>
-              <div class="uk-flex uk-flex-center">
-                <button class="uk-button uk-button-secondary" @click="removeImage">Remove</button>
-              </div>
+              <div class="uk-card-badge uk-label-danger">Feale</div>
             </div>
-        </div>
-        <!-- Show Predict Result -->
-        <div>
-          <div v-if="!character">
-            <div class="uk-child-width-1-1 uk-text-center" uk-grid>
-              <div class="uk-child-width-1-2 uk-padding-small" uk-grid>
-                <div>
-                  <img src="/static/who.png" width="80" alt="">
-                </div>
-                <div>
-                  <h3>Character</h3>
-                </div>
-              </div>
-              <div class="uk-child-width-1-2  uk-padding-small" uk-grid>
-                <div>
-                  <img src="/static/percent.png" width="80" alt="">
-                </div>
-                <div>
-                  <h3>Percent</h3>
-                </div>
-              </div>
-              <div class="uk-child-width-1-2  uk-padding-small" uk-grid>
-                <div>
-                  <img src="/static/time1.png" width="80" alt="">                  
-                </div>
-                <div>
-                  <h3>%</h3>
-                </div>
-              </div>
-            </div>
+
+            <h2>{{ character_name }}</h2>
+            <h4 class="uk-text-left">10歲的霸子是荷馬和美枝唯一的兒子，花枝和奶嘴的哥哥。霸子最引人注目的特點就是他淘氣、不服管教、不尊重權威的性格。</h4>
           </div>
-          <div class="uk-padding" v-else>
-            <div class="uk-child-width-1-1 uk-text-center uk-flex-center" uk-grid>
-              <div class="uk-child-width-1-2 uk-padding-small" uk-grid>
-                <div>
-                  <img src="/static/who.png" width="80" alt="">
-                </div>
-                <div>
-                  <h3>{{character}}</h3>
-                </div>
-              </div>
-              <div class="uk-child-width-1-2  uk-padding-small" uk-grid>
-                <div>
-                  <img src="/static/percent.png" width="80" alt="">
-                </div>
-                <div>
-                  <h3>{{percent}}%</h3>
-                </div>
-              </div>
-              <div class="uk-child-width-1-2  uk-padding-small" uk-grid>
-                <div>
-                  <img src="/static/time1.png" width="80" alt="">                  
-                </div>
-                <div>
-                  <h3>{{costTime}}</h3>
-                </div>
-              </div>
-            </div>
-          </div>
+
         </div>
+        <!-- <div class="uk-flex uk-flex-center">
+          <button class="uk-button uk-button-secondary" @click="removeImage">Remove</button>
+        </div> -->
       </div>
-      <!-- Introduction -->
-      <div class="uk-text-center">
-        <div class="bar uk-button uk-button-secondary uk-width-1-1 uk-light uk-text-center uk-position-relative">
-          <h3 class="uk-position-center">Introduction</h3>
-        </div>
-        <!-- <div class="rect"></div> -->
-      </div>
-      <div>
-        <div class="bar uk-button uk-button-secondary uk-width-1-1 uk-light uk-text-center uk-margin-top uk-position-relative">
-          <h3 class="uk-position-center">Visualize CNN</h3>
-        </div>
-        <!-- <div class="rect"></div> -->
+
+    </div>
+
+    <div class="uk-section uk-section-secondary bottom_info">
+      <div class="uk-container uk-text-center">
+        <h3>國立台北科技大學 - 電機工程系 - 高效能計算與深度學習研究室</h3>
+        <h3>National Taipei University of Technology - Department of Electrical Engineering</h3>
       </div>
     </div>
   </div>
@@ -106,9 +59,28 @@
 import * as tf from '@tensorflow/tfjs'
 
 const MODEL_PATH = 'static/web_tfjs_model/model.json'
-const Character = ['荷熊·辛普森', '阿普·納哈薩皮馬佩蒂隆', '霸子·辛普森', '郭董', '維古姆警官', '宅神', '埃德娜·克拉巴佩爾',
-  '荷馬·辛普森', '李掏', '小丑庫斯提', '倫尼·倫納德', '莉莎·辛普森', '美枝·辛普森', '郝融冰', '米爾豪斯·范霍滕',
-  '莫少蔥', '魯肉王', '阿浮', '西蒙·喜金捏', '包添丁']
+const Characters = [
+  {name: '荷熊·辛普森', gender: 'Male'},
+  {name: '阿普·納哈薩皮馬佩蒂隆', gender: 'Male'},
+  {name: '霸子·辛普森', gender: 'Male'},
+  {name: '郭董', gender: 'Male'},
+  {name: '維古姆警官', gender: 'Male'},
+  {name: '宅神', gender: 'Male'},
+  {name: '埃德娜·克拉巴佩爾', gender: 'Female'},
+  {name: '荷馬·辛普森', gender: 'Male'},
+  {name: '李掏', gender: 'Male'},
+  {name: '小丑庫斯提', gender: 'Male'},
+  {name: '倫尼·倫納德', gender: 'Male'},
+  {name: '莉莎·辛普森', gender: 'Female'},
+  {name: '美枝·辛普森', gender: 'Female'},
+  {name: '郝融冰', gender: 'Male'},
+  {name: '米爾豪斯·范霍滕', gender: 'Male'},
+  {name: '莫少蔥', gender: 'Male'},
+  {name: '魯肉王', gender: 'Male'},
+  {name: '阿浮', gender: 'Male'},
+  {name: '西蒙·喜金捏', gender: 'Male'},
+  {name: '包添丁', gender: 'Male'}
+]
 
 const loadModel = async () => {
   console.log('loading model...')
@@ -123,9 +95,11 @@ export default {
   data () {
     return {
       image: '',
-      character: '',
+      character_name: '',
       percent: '',
-      costTime: ''
+      costTime: '',
+      character_gender: 'Male',
+      isPredicted: false
     }
   },
   methods: {
@@ -175,31 +149,25 @@ export default {
         var idx = predication.argMax(1).dataSync()[0]
         const max = predication.max().dataSync()[0]
         this.percent = max.toFixed(5) * 100
-        this.character = Character[idx]
+        this.character_name = Characters[idx].name
         this.costTime = (end - start) / 1000 + ' Sec'
+        this.character_gender = Characters[idx].gender
+        this.isPredicted = true
       })
     },
     removeImage () {
       this.image = ''
-      this.character = ''
+      this.character_name = ''
       this.costTime = ''
       this.percent = ''
+      this.character_gender = ''
+      this.isPredicted = false
     }
   }
 }
 </script>
 
 <style>
-html, body{
-  height: 100%;
-}
-/* .bg {
-  background: url("/static/730131.jpg");
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  height: 100%;
-} */
 .move img{
   animation: floating 2s ease infinite;
 }
@@ -218,17 +186,11 @@ html, body{
   height: 300px;
   text-align: center;
 }
-h3{
-  margin: 0;
+.bottom_info h3{
+  font-family: SimSun, Microsoft JhengHei;
 }
-.bar{
-  height: 50px;
+.character_introduction h2, h4{
+  font-family: SimSun, Microsoft JhengHei;
 }
-/* .rect{
-  height: 0;
-  width: 0;
-  border-color: #222 transparent transparent transparent;
-  border-style: solid;
-  border-width: 20px;
-} */
+
 </style>
